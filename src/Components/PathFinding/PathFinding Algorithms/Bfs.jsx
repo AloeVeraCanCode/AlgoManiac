9c4efd9d523816ff2791e0a0e912d8f00659925c
Akash <thatsakashpal@gmail.com>
2024-04-25 18:30:53 +0530

    BFS complete

---
+++ src/Components/PathFinding/PathFinding Algorithms/Bfs.jsx
@@ -0,0 +1,86 @@
+
+var ROW = 25;
+var COL = 40;
+var grid=[],ret=[],endPoints=[];
+var dRow = [0, 1, 0, -1];
+var dCol = [ -1, 0, 1, 0];
+var vis = Array.from(Array(ROW), ()=> Array(COL).fill(false));
+function isValid(vis, row, col)
+{
+	// If cell lies out of bounds
+	if (row < 0 || col < 0
+		|| row >= ROW || col >= COL|| grid[row][col].color=='black')
+		return false;
+
+	// If cell is already visited
+	if (vis[row][col])
+		return false;
+
+	// Otherwise
+	return true;
+}
+
+// Function to perform the BFS traversal
+function BFS( row, col)
+{
+	var par=new Map();
+	var q = [];
+
+	// Mark the starting cell as visited
+	// and push it into the queue
+    var turn=0;
+	q.push([row, col ,turn ]);
+	vis[row][col] = true;
+    par[[row,col]]=[-1,-1,-1];
+    vis[row][col]=true;
+	// Iterate while the queue
+	// is not empty
+	while (q.length!=0) {
+
+		var cell = q[0];
+		var x = cell[0];
+		var y = cell[1];
+        var z= cell[2];
+        var row=x,col=y;
+		q.shift();
+        ret.push([x,y,z]);
+        if(endPoints[1][0]==row&&endPoints[1][1]==col)break;
+		// Go to the adjacent cells
+		for (var i = 0; i < 4; i++) {
+
+			var adjx = x + dRow[i];
+			var adjy = y + dCol[i];
+
+			if (isValid(vis, adjx, adjy)) {
+				q.push([adjx, adjy, z+1 ]);
+				vis[adjx][adjy] = true;
+                if(i==0)
+                par[[adjx,adjy]]=[row,col,"la"];
+                else if(i==1)
+                par[[adjx,adjy]]=[row,col,"da"];
+                else if(i==2)
+                par[[adjx,adjy]]=[row,col,"ra"];
+                else if(i==3)
+                par[[adjx,adjy]]=[row,col,"ua"];
+			}
+		}
+	}
+    var sr=[],cur=[endPoints[1][0],endPoints[1][1]];
+    console.log(par);    console.log(cur);console.log(par[[cur[0],cur[1]]]);
+    while(par[[cur[0],cur[1]]][0]!=-1)
+    {
+        sr.push(par[[cur[0],cur[1]]]);
+        cur=[par[[cur[0],cur[1]]][0],par[[cur[0],cur[1]]][1]];
+    }
+    console.log(sr);
+    return([ret,sr]);
+}
+
+// Driver Code
+// Given input matrix
+export default async function callBfs(g,e)
+{
+    console.log(g);
+    grid=g;endPoints=e;
+    return(BFS(endPoints[0][0],endPoints[0][1]));
+}
--- src/Components/PathFinding/PathFindingBar.jsx
+++ src/Components/PathFinding/PathFindingBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState,useContext } from 'react';
 import '../../CSS/PathFinding/PathFindingBar.css';
 import callDfs from './PathFinding Algorithms/Dfs.jsx';
+import callBfs from './PathFinding Algorithms/Bfs.jsx';
 import {Context,  GridContext } from '../../Pages/PathFinding/GridContext.jsx';
 import da from '../../images/down arrow.png';
 import la from '../../images/left arrow.png';
@@ -20,6 +21,24 @@ export default function (props) {
         let z=Array.from(grid);
         console.log(populate[j][0]+" "+populate[j][1]);
         z[populate[j][0]][populate[j][1]].isAnimate="id2";
+        setGrid(z);
+        console.log("hehe");
+        console.log(z);
+        await wait(50);
+        
+      };
+    }
+    async function animateBfs(populate,setGrid)
+    {
+      
+      for(let j=0;j<populate.length;){
+        let z=Array.from(grid);
+        var cur=populate[j][2];
+        while(j<populate.length&&cur==populate[j][2]){
+        console.log(populate[j][0]+" "+populate[j][1]);
+        z[populate[j][0]][populate[j][1]].isAnimate="id2";
+        j++;
+        }
         setGrid(z);
         console.log("hehe");
         console.log(z);
@@ -83,6 +102,14 @@ export default function (props) {
         await showPath(ActualPath,props.setGrid);
         console.log(grid);
       }
+      else if(method=='BFS')
+      {
+        var [traversedPath,ActualPath]=await callBfs(grid,props.endPoints);
+        console.log(traversedPath)
+        await animateBfs(traversedPath,props.setGrid);
+        await showPath(ActualPath,props.setGrid);
+        console.log(grid);
+      }
       enablePointers(); 
      };
    return (
@@ -92,8 +119,8 @@ export default function (props) {
          <a href="/"><button type="submit" href="/">Home</button></a>
          <button href="/" id="random" onClick={setArray}>Randomize</button>
          <select name="DFS" id="pathFindingMethods">
-             <option value="">DFS</option>
              <option value="">BFS</option>
+             <option value="">DFS</option>
          </select>
        </div>
        <button className="find-path-button"  id='findPathBtn' onClick={findPath}>Find Path</button>
